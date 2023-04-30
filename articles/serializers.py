0a76fe84298c4f87e8481910609ed09074e5a6bf
Wang Gyuwon <wgyuwon@gmail.com>
2023-05-01 02:40:36 +0900

    third commit

---
+++ articles/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from articles.models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    def get_user(self, obj):
+        return obj.user.email
+
+    class Meta:
+        model = Article
+        fields = ("pk", "title", "image", "created_at", "updated_at", "user")
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ("title", "image", "content")
